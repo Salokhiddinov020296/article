09895a6604e38588d1390374686a5b77d19400e2
Maksud <saloxiddinov.1996@mail.ru>
2022-09-20 08:44:56 +0500

    ...

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'articledb',
         'USER': 'articleuser',
         'PASSWORD': 'articlepassword',
